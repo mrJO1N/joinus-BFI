b4b1616dbff0570f6eab41c99e05a0da71d2048b
mrJO1N <66362958+mrJO1N@users.noreply.github.com>
2023-05-24 23:13:04 +0300

    0.2.0

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
-import { input, interpret, setBFProgrammCode } from "./interpret.js";
+import {
+  input,
+  interpret,
+  setBFProgrammCode,
+  getOutput,
+  resetState,
+} from "./interpret.js";
 
 const inputCodeElem = document.querySelector("#code"),
   inputElem = document.querySelector("#input"),
   outputElem = document.querySelector("output");
 
 document.querySelector(".inputs").addEventListener("keyup", () => {
+  resetState();
   setBFProgrammCode(inputCodeElem.value);
   input(inputElem.value);
-  outputElem.innerText = interpret("lol") ?? "";
+  interpret();
+  outputElem.innerText = getOutput();
 });
 
 console.log("interpret------RUN");
