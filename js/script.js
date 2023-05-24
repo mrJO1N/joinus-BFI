c47e173f36b02979cc9d9346c120dfb57c0e12fa
mrJO1N <zhenduk.zhenduk123456780@gmail.com>
2023-05-24 22:06:51 +0300

    0.0.2

--- js/script.js
+++ js/script.js
@@ -1,22 +1,54 @@
 import { input, interpret, setBFProgrammCode } from "./interpret.js";
 
 const inputCodeElem = document.querySelector("#code"),
-  InputElem = document.querySelector("#input"),
+  inputElem = document.querySelector("#input"),
+  outputElem = document.querySelector("output");
+
+/*
+  
+  
   canv = document.querySelector("canvas"),
   ctx = canv.getContext("2d");
 canv.height = window.innerHeight;
 canv.width = 700;
+ctx.font = "50pt Arial";
+
+function wrapText(text) {
+  ctx.clearRect(0, 0, canv.width, canv.height);
+  const marginLeft = 20,
+    marginTop = 50,
+    words = text.split(" "),
+    countWords = words.length,
+    lineHeight = 140;
+  let line = "";
+  for (var n = 0; n < countWords; n++) {
+    var testLine = line + words[n] + " ";
+    var testWidth = ctx.measureText(testLine).width;
+    if (testWidth > canv.width) {
+      ctx.fillText(line, marginLeft, marginTop);
+      line = words[n] + " ";
+      marginTop += lineHeight;
+    } else {
+      line = testLine;
+    }
+  }
+  ctx.fillText(line, marginLeft, marginTop);
+}
+
+
+
+
+
+*/
 
 function output(text) {
-  ctx.clearRect(0, 0, canv.width, canv.height);
-  ctx.font = "italic 30pt Arial";
-  console.log(ctx.measureText(text));
+  outputElem.innerText = text;
 }
 
 document.querySelector(".inputs").addEventListener("keyup", () => {
   setBFProgrammCode(inputCodeElem.value);
-  input(InputElem.value);
-  output(interpret() ?? "");
+  input(inputElem.value);
+  output(interpret("lol") ?? "");
 });
 
 console.log("interpret------RUN");
