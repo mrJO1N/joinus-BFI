52b6ff9d064d12a1263bd1f6e3f5733692b5342f
mrJO1N <66362958+mrJO1N@users.noreply.github.com>
2023-05-24 23:12:52 +0300

    0.2.0

--- js/interpret.js
+++ js/interpret.js
@@ -1,11 +1,11 @@
 /** Interpreter variables */
 // Create a new 30,000 size array, with each cell initialized with the value of 0. Memory can expand.
 const MEMORY_SIZE = 30_000,
-  memory = new Array(MEMORY_SIZE).fill(0),
-  // Address stack. Used to track addresses (index) of left brackets
-  astack = [];
+  memory = new Array(MEMORY_SIZE).fill(0);
+// Address stack. Used to track addresses (index) of left brackets
 
 let programmStr = "",
+  astack = [],
   inputStr = "",
   outputStr = "",
   //Instruction pointer (Points to the current INSTRUCTION)
@@ -21,11 +21,42 @@ function input(localInputStr) {
   inputStr = localInputStr;
 }
 
+function getInput() {
+  // Set a default value to return in case there is no input to consume
+  let val = 0;
+
+  // If input isn't empty
+  if (inputStr) {
+    // Get the character code of the first character of the string
+    val = inputStr.charCodeAt(0);
+    // Remove the first character from the string as it is "consumed" by the program
+    inputStr = inputStr.substring(1);
+  }
+
+  return val;
+}
+
+function sendOutput(value) {
+  // if (value == 0) value = "0";
+  outputStr += String.fromCharCode(value);
+}
+
+function resetState() {
+  // Clear memory, reset pointers to zero.
+  memory.fill(0);
+  ipointer = 0;
+  mpointer = 0;
+  outputStr = "";
+  inputStr = "";
+  programmStr = "";
+  astack = [];
+}
+
 function interpret() {
   let end = false;
 
   while (!end) {
-    switch (program[ipointer]) {
+    switch (programmStr[ipointer]) {
       case ">":
         if (mpointer == memory.length - 1)
           /* If we try to access memory beyond what is currently available, expand array */
@@ -56,9 +87,9 @@ function interpret() {
           let count = 0;
           while (true) {
             ipointer++;
-            if (!program[ipointer]) break;
-            if (program[ipointer] === "[") count++;
-            else if (program[ipointer] === "]") {
+            if (!programmStr[ipointer]) break;
+            if (programmStr[ipointer] === "[") count++;
+            else if (programmStr[ipointer] === "]") {
               if (count) count--;
               else break;
             }
@@ -69,7 +100,7 @@ function interpret() {
         //Pointer is automatically incremented every iteration, therefore we must decrement to get the correct value
         ipointer = astack.pop() - 1;
         break;
-      case undefined: // We have reached the end of the program
+      case undefined: // We have reached the end of the programmStr
         end = true;
         break;
       default: // We ignore any character that are not part of regular Brainfuck syntax
@@ -77,8 +108,12 @@ function interpret() {
     }
     ipointer++;
   }
-  console.log(output);
-  return output;
+  console.log(outputStr);
+  return outputStr;
 }
 
-export { setBFProgrammCode, input, interpret };
+function getOutput() {
+  return outputStr;
+}
+
+export { setBFProgrammCode, input, interpret, getOutput, resetState };
