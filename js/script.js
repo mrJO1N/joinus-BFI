88ee6826d630b218b02b5a5e8ec5e907038372ce
mrJO1N <66362958+mrJO1N@users.noreply.github.com>
2023-05-24 22:29:55 +0300

    0.0.4

--- js/script.js
+++ js/script.js
@@ -4,51 +4,10 @@ const inputCodeElem = document.querySelector("#code"),
   inputElem = document.querySelector("#input"),
   outputElem = document.querySelector("output");
 
-/*
-  
-  
-  canv = document.querySelector("canvas"),
-  ctx = canv.getContext("2d");
-canv.height = window.innerHeight;
-canv.width = 700;
-ctx.font = "50pt Arial";
-
-function wrapText(text) {
-  ctx.clearRect(0, 0, canv.width, canv.height);
-  const marginLeft = 20,
-    marginTop = 50,
-    words = text.split(" "),
-    countWords = words.length,
-    lineHeight = 140;
-  let line = "";
-  for (var n = 0; n < countWords; n++) {
-    var testLine = line + words[n] + " ";
-    var testWidth = ctx.measureText(testLine).width;
-    if (testWidth > canv.width) {
-      ctx.fillText(line, marginLeft, marginTop);
-      line = words[n] + " ";
-      marginTop += lineHeight;
-    } else {
-      line = testLine;
-    }
-  }
-  ctx.fillText(line, marginLeft, marginTop);
-}
-
-
-
-
-
-*/
-
-function output(text) {
-  outputElem.innerText = text;
-}
-
 document.querySelector(".inputs").addEventListener("keyup", () => {
   setBFProgrammCode(inputCodeElem.value);
   input(inputElem.value);
-  output(interpret("lol") ?? "");
+  outputElem.innerText = interpret("lol") ?? "";
 });
 
 console.log("interpret------RUN");
